7ac6048c595459d386ab04f00b5e66ae1bf222d1
Trevor Abram <trevor@fideagency.com>
2019-02-16 23:13:53 -0600

    Added crud actions to quote controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,35 +5,44 @@ class QuotesController < ApplicationController
     render json: {status: "SUCCESS", message: "You've loaded quotes", data:quotes}, status: :ok
   end
 
-  # def show
-  #   @quotes = Quote.find(params[:id])
-  #   json_response(@quote)
-  # end
-  #
-  # def create
-  #   @quotes = Quote.create(quote_params)
-  #   json_response(@quote)
-  # end
-  #
+  def show
+    quote = Quote.find(params[:id])
+    render json: {status: "SUCCESS", message: "You've loaded quotes", data:quote}, status: :ok
+  end
+
+  def create
+    quote = Quote.create(quote_params)
+    # Create an if statement unprocessable entities
+    if quote.save
+      render json: {status: "SUCCESS", message: "You've saved a quote", data:quote}, status: :ok
+    else
+      render json: {status: "Error", message: "Quote not saved", data:quote.errors}, status: :unprocessable_entity
+    end
+  end
+
   # def update
-  #   @quotes = Quote.find(params[:id])
-  #   @quote.update(quote_params)
+  #   quote = Quote.find(params[:id])
+  #   if quote.update_attributes(quote_params)
+  #     render json: {status: "SUCCESS", message: "You've updated the goods", data:quote}, status: :ok
+  #   else
+  #     render json: {status: "Error", message: "Did not update"}, data:quote}, status: :unprocessable_entity
+  #   end
   # end
-  #
-  # def destroy
-  #   @quotes = Quote.find(params[:id])
-  #   @quote.destroy
-  # end
-  #
-  #
-  # private
-  # def json_response(object, status = :ok)
-  #   # Could also list 200 as status code. Changing to 401 for example will perform a successful, but unauthorized call.
-  #   render json: object, status: :ok
-  # end
-  #
-  #
-  # def quote_params
-  #   params.permit(:author, :content)
-  # end
+
+
+  def destroy
+    quote = Quote.find(params[:id])
+    quote.destroy
+    render json: {status: "SUCCESS", message: "Quote removed.", data:quote}, status: :ok
+  end
+
+  private
+  def json_response(object, status = :ok)
+    # Could also list 200 as status code. Changing to 401 for example will perform a successful, but unauthorized call.
+    # render json: status: :ok
+  end
+
+  def quote_params
+    params.permit(:author, :content)
+  end
 end
